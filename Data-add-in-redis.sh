d5e83edc6edb7c6c261f22a2490521b869f25d5c
sivasai4632 <sivasai4632@gmail.com>
2022-01-11 10:54:35 +0000

    ttt

--- Data-add-in-redis.sh
+++ Data-add-in-redis.sh
@@ -1,4 +1,4 @@
-#Use_id & score will come from lb-3663553998042595328-2021-12-18 file.##
+#Use_id & score will come from lb-3663553998042595328-2021-12-18 file.
 while read line
 do
 user_id=`echo $line | awk -F',' '{print $1}'`
